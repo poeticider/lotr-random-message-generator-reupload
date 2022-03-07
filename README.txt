c2189e797c149a041962db3b8bdc241f8f18c745
Dan <orange2k@hotmail.co.uk>
2022-03-07 17:26:49 +0000

    random word generator js and readme.txt

---
+++ README.txt
@@ -0,0 +1,14 @@
+plan/notes: random text generator
+
+random texts
+
+1 greeting (Greetings./Mr. Frodo!/So it begins./Oh, Sam.)
+
+2 comment (they're taking the hobbits to Isengard!/
+tell me, where is Gandalf, for I must desire to speak with him?/
+/PO -TA -TOES??/HANDS OFF THAT SHINY SHIRT, THAT'S MINE!)
+
+3 farewell (Leave now, and never come back!/It is a gift!/I was there, 3,000 years ago.../what did you say??)
+
+4 rating: golem/hobbit/legolas/Gandalf
+(your rating is: ****)
---
+++ script.js
@@ -0,0 +1,43 @@
+const rdmLotrmsg = {
+    _msgSection: {
+        greeting: ['Greetings.', 'Mr. Frodo!', 'So it begins.', 'Oh, Sam.'],
+        comment: ["They're taking the hobbits to Isengard!",
+        'Tell me, where is Gandalf, for I must desire to speak with him?',
+        'PO -TA -TOES??', "HANDS OFF THAT SHINY SHIRT, THAT'S MINE!"],
+        farewell: ['Leave now, and never come back!', 'It is a gift!', 'I was there, 3,000 years ago...', 'what did you say??']
+    },
+    get greeting() {
+        return this._msgSection.greeting
+    },
+    get comment() {
+        return this._msgSection.comment
+    },
+    get farewell() {
+        return this._msgSection.farewell
+    },
+    get msg() {
+        return {
+            greeting: this.greeting,
+            comment: this.comment,
+            farewell: this.farewell
+        };
+    },
+    getRdmMsg(section) {
+        const msgSection = this._msgSection[section];
+        const randomIndex = Math.floor(Math.random() * msgSection.length);
+        return msgSection[randomIndex];
+    },
+    generateRdmMsg() {
+        const  greeting = this.getRdmMsg('greeting');
+        const comment = this.getRdmMsg('comment');
+        const farewell = this.getRdmMsg('farewell');
+        return `${greeting} ${comment} ${farewell}`
+    }
+};
+
+const lotrRandomMessage = rdmLotrmsg.generateRdmMsg();
+console.log(lotrRandomMessage);
+
+
+
+
